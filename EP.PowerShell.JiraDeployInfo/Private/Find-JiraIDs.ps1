b4db8e79aa698fa06cae7f6965dce382adc02a2a
Michaela Wheeler <michaela.wheeler@educationperfect.com>
2024-02-09 09:09:13 +1100

    Update JIRA regexp

--- EP.PowerShell.JiraDeployInfo/Private/Find-JiraIDs.ps1
+++ EP.PowerShell.JiraDeployInfo/Private/Find-JiraIDs.ps1
@@ -2,7 +2,7 @@ function Find-JiraIDs {
     param (
         $message
     )
-    $pattern = '[A-Z]{2,}+-\d{1,10}'
+    $pattern = '\b[A-Za-z][A-Za-z0-9_]+-[1-9][0-9]*\b'
     $values = [regex]::Matches($message, $pattern) | Select-Object value 
     Write-Debug ("[JIRA IDs] " + $values)
     $values
